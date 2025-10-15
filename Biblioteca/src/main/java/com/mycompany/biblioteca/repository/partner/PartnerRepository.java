b02520995f28b493598d0fb8ef7a17b1822693c2
Coder <Coder@usuario-Saturno-Series>
2025-10-14 19:26:01 -0500

    feat: creating file  repository partner

---
+++ Biblioteca/src/main/java/com/mycompany/biblioteca/repository/partner/PartnerRepository.java
@@ -0,0 +1,8 @@
+package com.mycompany.biblioteca.repository.partner;
+
+import com.mycompany.biblioteca.model.Partner;
+import com.mycompany.biblioteca.repository.Repository;
+
+public interface PartnerRepository extends Repository<Partner, Integer> {
+
+}
