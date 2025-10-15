e5c96d06eb04e593b45829cb300629299bedad4b
Coder <Coder@usuario-Saturno-Series>
2025-10-14 19:24:21 -0500

    feat:Create repositoryBook

---
+++ Biblioteca/src/main/java/com/mycompany/biblioteca/repository/book/BookRepository.java
@@ -0,0 +1,8 @@
+package com.mycompany.biblioteca.repository.book;
+
+import com.mycompany.biblioteca.model.Book;
+import com.mycompany.biblioteca.repository.Repository;
+
+public interface BookRepository extends Repository<Book, Integer> {
+
+}
