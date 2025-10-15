cba7dde4a83e25f065bdc8839dc86ab15633143e
Coder <Coder@usuario-Saturno-Series>
2025-10-14 19:22:35 -0500

    feat:Create exepcion systemError

---
+++ Biblioteca/src/main/java/com/mycompany/biblioteca/exeptions/ErrorSystemException.java
@@ -0,0 +1,9 @@
+package com.mycompany.biblioteca.exeptions;
+
+public class ErrorSystemException extends RuntimeException {
+
+    public ErrorSystemException(String message) {
+        super(message);
+    }
+
+}
