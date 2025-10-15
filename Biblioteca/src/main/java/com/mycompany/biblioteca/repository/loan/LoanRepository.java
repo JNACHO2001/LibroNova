72ef0515c111f9b57ff3ae201f84b6180cd34670
Coder <Coder@usuario-Saturno-Series>
2025-10-14 19:25:50 -0500

    feat: creating file  repository loan

---
+++ Biblioteca/src/main/java/com/mycompany/biblioteca/repository/loan/LoanRepository.java
@@ -0,0 +1,8 @@
+package com.mycompany.biblioteca.repository.loan;
+
+import com.mycompany.biblioteca.model.Loan;
+import com.mycompany.biblioteca.repository.Repository;
+
+public interface LoanRepository extends Repository<Loan, Integer>{
+
+}
